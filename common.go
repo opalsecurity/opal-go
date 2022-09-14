700af0f88535f464a6fb1cafe87c2e38d476313c
Stephen Wan <stephen.wan@opal.dev>
2022-09-14 17:17:49 -0500

    make opal-go build (#7)
    * common.go: update helpers
    
    These handwritten helpers were not update with the generated changes.
    
    * go.mod tidy
    
    * .github: run go build and test

--- common.go
+++ common.go
@@ -1,28 +1,36 @@
-/* 
-    Utils to facilitate easier usage of the SDK.
+/*
+   Utils to facilitate easier usage of the SDK.
 */
 package opal
 
 func GroupToUpdateGroupInfo(group Group) (updateGroupInfo UpdateGroupInfo) {
-    return UpdateGroupInfo{
-        GroupId: group.GroupId,
-        Description: group.Description,
-        OwnerTeamId: group.OwnerTeamId,
-        Visibility: group.Visibility,
-        MaxDuration: group.MaxDuration,
-        RequireManagerApproval: group.RequireManagerApproval,
-        RequireSupportTicket: group.RequireSupportTicket,
-    }
+	return UpdateGroupInfo{
+		GroupId:                group.GroupId,
+		Description:            group.Description,
+		AdminOwnerId:           group.AdminOwnerId,
+		MaxDuration:            group.MaxDuration,
+		RequireManagerApproval: group.RequireManagerApproval,
+		RequireSupportTicket:   group.RequireSupportTicket,
+		Name:                   group.Name,
+		FolderId:               group.FolderId,
+		RequireMfaToApprove:    group.RequireMfaToApprove,
+		AutoApproval:           group.AutoApproval,
+		RequestTemplateId:      group.RequestTemplateId,
+	}
 }
 
 func ResourceToUpdateResourceInfo(resource Resource) (updateResourceInfo UpdateResourceInfo) {
-    return UpdateResourceInfo{
-        ResourceId: resource.ResourceId,
-        Description: resource.Description,
-        OwnerTeamId: resource.OwnerTeamId,
-        Visibility: resource.Visibility,
-        MaxDuration: resource.MaxDuration,
-        RequireManagerApproval: resource.RequireManagerApproval,
-        RequireSupportTicket: resource.RequireSupportTicket,
-    }
+	return UpdateResourceInfo{
+		ResourceId:             resource.ResourceId,
+		Description:            resource.Description,
+		AdminOwnerId:           resource.AdminOwnerId,
+		MaxDuration:            resource.MaxDuration,
+		RequireManagerApproval: resource.RequireManagerApproval,
+		RequireSupportTicket:   resource.RequireSupportTicket,
+		Name:                   resource.Name,
+		FolderId:               resource.FolderId,
+		RequireMfaToApprove:    resource.RequireMfaToApprove,
+		AutoApproval:           resource.AutoApproval,
+		RequestTemplateId:      resource.RequestTemplateId,
+	}
 }
